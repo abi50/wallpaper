dc5421c5e842f9059596dd0d0c63100e0767f4ac
abigail <abigail506040@gmail.com>
2024-07-31 23:27:54 +0300

    work fine

--- server/DB/mongoConect.js
+++ server/DB/mongoConect.js
@@ -1,32 +1,14 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://gili:wallpapers@cluster0.uuibaeu.mongodb.net/wallpaper', {
-            // remove useNewUrlParser and useUnifiedTopology as they are deprecated
-        });
-        console.log('MongoDB connected...');
+        await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('MongoDB connected');
     } catch (err) {
-        console.error(err.message);
-        process.exit(1);
+        console.error('MongoDB connection error:', err);
+        process.exit(1); // Exit the process with failure
     }
 };
 
 export { connectToDB };
-// // mongoConnect.js
-// const mongoose = require('mongoose');
-
-// const connectToDB = async () => {
-//     try {
-//         await mongoose.connect('your-mongodb-uri-here', {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-//         console.log('MongoDB connected...');
-//     } catch (err) {
-//         console.error(err.message);
-//         process.exit(1);
-//     }
-// };
-
-//  export default connectToDB;
